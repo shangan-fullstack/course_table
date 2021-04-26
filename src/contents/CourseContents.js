13b72dd8872c125d46a24085319bc7d6e6c3f6f3
Yixuan Nie <yixuannie@Yixuans-MBP.attlocal.net>
2021-04-25 23:41:55 -0700

    add P12

--- src/contents/CourseContents.js
+++ src/contents/CourseContents.js
@@ -2,10 +2,12 @@ import {P8} from './full-stack/P8';
 import {P9} from './full-stack/P9';
 import {P10} from "./full-stack/P10";
 import {P11} from "./full-stack/P11";
+import {P12} from './full-stack/P12';
 
 export const CourseContents = {
   8: P8,
   9: P9,
   10: P10,
   11: P11,
+  12: P12,
 };
