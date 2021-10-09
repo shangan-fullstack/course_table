0c6519304e6d87e4198369f88b9022aa06d1ba55
K Chen <chenmingyue85@gmail.com>
2021-10-09 01:21:53 -0700

    Update CoueseConntents.js to include P20

--- src/contents/CourseContents.js
+++ src/contents/CourseContents.js
@@ -9,6 +9,7 @@ import {P15} from "./full-stack/P15";
 import {P17} from "./full-stack/P17";
 import {P18} from "./full-stack/P18";
 import {P19} from "./full-stack/P19";
+import {P20} from "./full-stack/P20";
 
 export const CourseContents = {
   8: P8,
@@ -22,4 +23,5 @@ export const CourseContents = {
   17: P17,
   18: P18,
   19: P19,
+  20: P20,
 };
