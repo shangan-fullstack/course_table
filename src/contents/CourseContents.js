2376840462c7eec40f86ea7f0ff70be5847f8d49
Yugan <shangan.xiaoyugan@gmail.com>
2021-10-10 16:26:20 -0700

    Remove old course tables

--- src/contents/CourseContents.js
+++ src/contents/CourseContents.js
@@ -1,25 +1,9 @@
-import {P8} from './full-stack/P8';
-import {P9} from './full-stack/P9';
-import {P10} from "./full-stack/P10";
-import {P11} from "./full-stack/P11";
-import {P12} from './full-stack/P12';
-import {P13} from "./full-stack/P13";
-import {P14} from "./full-stack/P14";
-import {P15} from "./full-stack/P15";
 import {P17} from "./full-stack/P17";
 import {P18} from "./full-stack/P18";
 import {P19} from "./full-stack/P19";
 import {P20} from "./full-stack/P20";
 
 export const CourseContents = {
-  8: P8,
-  9: P9,
-  10: P10,
-  11: P11,
-  12: P12,
-  13: P13,
-  14: P14,
-  15: P15,
   17: P17,
   18: P18,
   19: P19,
