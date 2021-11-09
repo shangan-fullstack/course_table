65f7755d8828a6e5ccdb953a1e41d16501498763
Yugan <shangan.xiaoyugan@gmail.com>
2021-11-09 14:12:18 -0800

    Add P21

--- src/contents/CourseContents.js
+++ src/contents/CourseContents.js
@@ -2,10 +2,12 @@ import {P17} from "./full-stack/P17";
 import {P18} from "./full-stack/P18";
 import {P19} from "./full-stack/P19";
 import {P20} from "./full-stack/P20";
+import {P21} from "./full-stack/P21";
 
 export const CourseContents = {
   17: P17,
   18: P18,
   19: P19,
   20: P20,
+  21: P21,
 };
