91efe30a8e4601650fcf739716043f704835bdbe
shangan-lizi <cli951103@gmail.com>
2022-01-28 23:39:52 -0800

    新版Java2022课程表

--- src/contents/CourseContents.js
+++ src/contents/CourseContents.js
@@ -1,10 +1,11 @@
-import {P17} from "./full-stack/P17";
-import {P18} from "./full-stack/P18";
-import {P19} from "./full-stack/P19";
-import {P20} from "./full-stack/P20";
-import {P21} from "./full-stack/P21";
-import {P22} from "./full-stack/P22";
-import {P23} from "./full-stack/P23";
+import { P17 } from "./full-stack/P17";
+import { P18 } from "./full-stack/P18";
+import { P19 } from "./full-stack/P19";
+import { P20 } from "./full-stack/P20";
+import { P21 } from "./full-stack/P21";
+import { P22 } from "./full-stack/P22";
+import { P23 } from "./full-stack/P23";
+import { P24 } from "./full-stack/P24";
 
 export const CourseContents = {
   17: P17,
@@ -14,4 +15,5 @@ export const CourseContents = {
   21: P21,
   22: P22,
   23: P23,
+  24: P24
 };
