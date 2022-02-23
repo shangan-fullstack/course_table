9a56c77d77c90f9c3fa95f70d1c39e882559723f
K Chen <chenmingyue85@gmail.com>
2022-02-22 22:56:57 -0800

    新版python

---
+++ src/contents/full-stack/toc/python/V2021_1.js
@@ -0,0 +1,133 @@
+export const V2021_2 = {
+  2: {
+    type: "non_expirement",
+    title: "第二章 JS基础以及HTML, CSS",
+    topics: [
+      "JS是如何引领前端开发数十年的？",
+      "什么是npm,  node.js? 常用包？ 如何部署环境？",
+      "JS基础数据类型, 循环、分支、解构赋值语法以及声明方式",
+      "JS原生数据结构",
+      "函数式编程与箭头函数",
+      "什么是 DOM？",
+      "HTML的行状元素，块状元素与可变元素",
+      "CSS中的盒模型与flex布局",
+    ]
+  },
+  3: {
+    type: "non_expirement",
+    title: "第三章 React",
+    topics: [
+      "什么是React? 如何用OOD思想去理解一个Component?",
+      "React 中的生命周期以及状态",
+      "React Hook 是想要给前端带来何种变革？",
+      "如何在React.js 以及 React Hook中进行开发",
+    ]
+  },
+  4: {
+    type: "expirement",
+    title: "实验课1",
+    subtitle: "选课系统前端部分1 - 基础页面的展示",
+    topics: [
+      "选课系统前端实验课分为两个部分，本节实验课是用React框架重构实验课1的内容，引入了React Router, axios, Material UI等现代框架/包替换原生解决方案.",
+      "实践React工作流程(workflow)",
+      "实战配置React router，分析React router带来的好处",
+      "使用Material UI作为CSS库美化页面",
+    ]
+  },
+  5: {
+    type: "expirement",
+    title: "实验课2",
+    subtitle: "制作COVID case map",
+    topics: [
+      "state与props",
+      "创建一个React项目",
+      "React Hook"
+    ]
+  },
+  6: {
+    type: "non_expirement",
+    title: "第六章 Django",
+    topics: [
+      "Django 核心内容介绍",
+      "OOP核心原则：SOLID",
+      "创建一个Django项目",
+      "Django Model, View, Template (MVT)分别做什么?",
+      "Django中的routing",
+      "什么是RESTful API?什么是CRUD(增删改查)？",
+      "Django Rest Framework(DRF) 能为我们做什么？",
+    ]
+  },
+  7: {
+    type: "expirement",
+    title: "实验课3",
+    subtitle: "选课系统后端部分1: 设计RESTful API，返回in memory数据库的内容",
+    topics: [
+      "后端分为两部分，本节实验课重点在实现Views与Routing，与RESTful API的设计实战。",
+      "安装后端环境和依赖。为何使用Python Virtural Environment (venv)?",
+      "怎样设计RESTful API？",
+      "Django中怎样接收Request body? 怎样返回JSON?",
+      "运用DRF API来实现接收和返回",
+      "使用Postman测试接口",
+    ]
+  },
+  8: {
+    type: "non_expirement",
+    title: "第七章 Mysql与Django Models",
+    topics: [
+      "数据库基础：事务性与ACID原则",
+      "数据库基础：主键，外键与索引",
+      "数据库基础：三范式、一对多与多对多",
+      "Django绑定数据库",
+      "安装MySQL Client并与Django绑定数据库",
+      "Django.db Model 的ORM绑定",
+      "如何通过QuerySet进行CRUD操作？",
+      "如何通过创建serializer进行数据的序列化与反序列化？",
+      "配置Django Admin，利用Admin tool查看数据",
+    ]
+  },
+  9: {
+    type: "expirement",
+    title: "实验课4",
+    subtitle: "实现Django Models，并与MySQL数据库交互进行CRUD操作",
+    topics: [
+      "后端分为两部分，本节实验课重点在Models，即ORM绑定，以及Serializer的实现。以及后端总体流程串讲。",
+      "从零实战ORM绑定操作：主键绑定，外键绑定",
+      "通过Migrations将ORM应用到数据库中",
+      "利用DRF API创建Model Serializers，并应用到CRUD操作中",
+      "后端整合测试与后端流程总结",
+      "后端debug",
+    ]
+  },
+  10: {
+    type: "expirement",
+    title: "实验课5",
+    subtitle: "选课系统前端部分2 - 实现登录, cookies存储，前后端整合",
+    topics: [
+      "前端实验课分为三个部分，本节实验课是实现登录功能，以及对token的存储。",
+      "借助Reactor Router设计login界面",
+      "错误消息处理与展示，UI loading状态处理",
+      "什么是cookies？怎样存储，读取，销毁cookies",
+      "前端是如何连接后端的？全栈概念图串解",
+    ]
+  },
+  11: {
+    type: "expirement",
+    title: "实验课6",
+    subtitle: "选课系统前端部分3 - 实现选课，删课功能",
+    topics: [
+      "分析选课、删课的按钮功能应该由谁实现？如何优雅的传递回调函数？",
+      "错误消息处理与展示，UI loading状态处理",
+    ]
+  },
+  12: {
+    type: "non_expirement",
+    title: "第六章 系统设计以及优化",
+    topics: [
+      "优化的本质是压榨系统性能外加合理利用各种服务以及模型。",
+      "什么是Cache, 什么是Strategy? Cashe的种类？",
+      "什么是多线程？什么是同步异步？",
+      "如何设计一个系统？通常步骤是什么？",
+      "现场分组设计近期高频系统设计题并分析总结",
+    ]
+  },
+}
