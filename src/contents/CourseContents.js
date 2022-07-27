7b2209ed8670c77e6b72ffd2a78e4f6904d54598
shangan-lizi <cli951103@gmail.com>
2022-07-27 16:13:26 -0700

    p31

--- src/contents/CourseContents.js
+++ src/contents/CourseContents.js
@@ -12,6 +12,7 @@ import { P27 } from "./full-stack/P27";
 import { P28 } from "./full-stack/P28";
 import { P29 } from "./full-stack/P29";
 import { P30 } from "./full-stack/P30";
+import { P31 } from "./full-stack/P31";
 
 export const CourseContents = {
   17: P17,
@@ -28,4 +29,5 @@ export const CourseContents = {
   28: P28,
   29: P29,
   30: P30,
+  31: P31,
 };
